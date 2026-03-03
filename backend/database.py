038a0e7360494e33a00a4d475d2feae032a7397f
Ratmir <ska62@live.com>
2026-03-03 19:35:04 +0400

    Stable version after audit

--- backend/database.py
+++ backend/database.py
@@ -1,7 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-engine = create_engine('sqlite:///./safespace.db', connect_args={'check_same_thread': False})
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///e:/SafeSpace/safespace.db"
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={'check_same_thread': False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 def get_db():
